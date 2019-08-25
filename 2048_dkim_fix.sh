6654b5a506566e28eb8fe4693f543da408da407d
hhamilton-eig <48221769+hhamilton-eig@users.noreply.github.com>
2019-08-25 14:59:20 -0700

    Update 2048_dkim_fix.sh

--- 2048_dkim_fix.sh
+++ 2048_dkim_fix.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
 # Sets owner of domain argument
-domain_owner=$(/scripts/whoowns $1)
+domain_owner=$(/scripts/whoowns ${1})
 
-# Backs up zone
-cp /var/named/${1}.db{,.$(date +%s).bak}
-
-# Finds line number of default._domainkey line
-dkim_line=$(egrep -n "^default._domainkey\s" /var/named/$1.db | cut -d: -f1)
+# Backs up zone + removes existing DKIM
+sed -i.$(date +%s).bak '/default._domainkey\s/d' /var/named/${1}.db
 
 create_dkim() {
-openssl genrsa -out /var/cpanel/domain_keys/private/$1 2048 && \
-openssl rsa -in /var/cpanel/domain_keys/private/$1 -pubout -out \
-/var/cpanel/domain_keys/public/$1
-dkim_record="v=DKIM1;  k=rsa; p="$(awk '$0 !~ / KEY/{printf $0 }' /var/cpanel/domain_keys/public/$1 )\"
+openssl genrsa -out /var/cpanel/domain_keys/private/${1} 2048 && \
+openssl rsa -in /var/cpanel/domain_keys/private/${1} -pubout -out \
+/var/cpanel/domain_keys/public/${1}
+dkim_record="v=DKIM1; k=rsa; p="$(awk '$0 !~ / KEY/{printf $0 }' /var/cpanel/domain_keys/public/$1 )""
 }
 
 zone_edit_call() {
 cpapi2 --user=$domain_owner \
-ZoneEdit edit_zone_record \
-Line=$dkim_line \
+ZoneEdit add_zone_record \
 domain=$1 \
 name='default._domainkey' \
 type=TXT \
@@ -27,12 +23,28 @@ ttl=14400 \
 class=IN
 }
 
-create_dkim $1
+urlencodepipe() {
+  local LANG=C; local c; while IFS= read -r c; do
+    case $c in [a-zA-Z0-9.~_-]) printf "$c"; continue ;; esac
+    printf "$c" | od -An -tx1 | tr ' ' % | tr -d '\n'
+  done <<EOF
+$(fold -w1)
+EOF
+  echo
+}
+urlencode() { printf "$*" | urlencodepipe ;}
 
 # split dkim into dkim 1 and 2 here
+create_dkim $1 &>/dev/null
+dkim_chunk_1=${dkim_record:0:249}
+dkim_chunk_2=${dkim_record:250}
 
-zone_edit_call $1 #put dkim chunk 1 here
+# URL encodes dkim chunks
+dkim_chunk_1=$(urlencode $dkim_chunk_1)
+dkim_chunk_2=$(urlencode $dkim_chunk_2)
 
-dkim_line=$[dkim_line+1]
+# Pops dkim chunk 1 into zone file
+zone_edit_call $1 $dkim_chunk_1 &>/dev/null
 
-zone_edit_call $1 #put dkim chunk 2 here
+# Pops dkim chunk 2 into zone file below chunk 1
+zone_edit_call $1 $dkim_chunk_2 &>/dev/null
